342434f022bea247b352750b2d81c32f77685905
Nicholas <nicholasgalante94@gmail.com>
2022-11-22 07:02:27 -0500

    commit tree 9e5452495bb0fbb8f215d13e6ee40d2f5a500e1e

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastfavoriteCustomer = 'nick';
+
+upperCaseCustomerName = () => {
+    customerName = customerName.toUpperCase();
+}
+
+setBestCustomer = () => {
+    bestCustomer = 'not bob';
+}
+
+overwriteBestCustomer = () => {
+    bestCustomer = 'maybe bob';
+}
+
+changeLeastFavoriteCustomer = () => {
+    leastfavoriteCustomer = 'BOB!';
+}
